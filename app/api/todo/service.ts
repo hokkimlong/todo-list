923944cf9988960d11511382e226b48e86aaff69
hokkimlong <57717461+hokkimlong@users.noreply.github.com>
2023-08-21 21:15:12 +0700

    1 file modified

--- app/api/todo/service.ts
+++ app/api/todo/service.ts
@@ -1,9 +1,9 @@
-import { Todo } from "@/features/todo/type";
-import { createClient } from "@supabase/supabase-js";
+import { Todo } from '@/features/todo/type';
+import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.SUPABASE_URL as string,
-  process.env.SUPABASE_ANON_KEY as string,
+  'https://erfhbbmkzrnnamkbfuuw.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVyZmhiYm1renJubmFta2JmdXV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI1NDgyNjIsImV4cCI6MjAwODEyNDI2Mn0.1muUiYBRfmSkhXnYfEnOfnzpMYIxXbYpTdt6-rsd620',
   {
     auth: {
       persistSession: false, //or true
@@ -14,30 +14,30 @@ const supabase = createClient(
 const todoService = {
   async getTodos(query: { search: string | null }) {
     let { data } = await supabase
-      .from("todo")
-      .select("*")
-      .like("todo", `%${query.search}%`)
-      .order("id", { ascending: true });
+      .from('todo')
+      .select('*')
+      .like('todo', `%${query.search}%`)
+      .order('id', { ascending: true });
 
     return data as Todo[];
   },
   async getTodo({ todo }: { todo: string }) {
     let { data } = await supabase
-      .from("todo")
-      .select("*")
-      .eq("todo", todo)
+      .from('todo')
+      .select('*')
+      .eq('todo', todo)
       .maybeSingle();
 
     return data as Todo;
   },
   async createNewTodo(todo: Partial<Todo>) {
-    return supabase.from("todo").insert(todo);
+    return supabase.from('todo').insert(todo);
   },
-  async updateTodo(todo: Pick<Todo, "id" | "todo" | "isCompleted">) {
-    return supabase.from("todo").update(todo).eq("id", todo.id);
+  async updateTodo(todo: Pick<Todo, 'id' | 'todo' | 'isCompleted'>) {
+    return supabase.from('todo').update(todo).eq('id', todo.id);
   },
   async removeTodo(todoId: string) {
-    return supabase.from("todo").delete().eq("id", todoId);
+    return supabase.from('todo').delete().eq('id', todoId);
   },
 };
 
